4a19a2e690ff5f59639c8401bdbc9c36a423d272
clara <10088152+claraj@users.noreply.github.com>
2020-11-03 19:44:56 -0600

    fix typo

--- lmnop_project/settings.py
+++ lmnop_project/settings.py
@@ -137,6 +137,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-# Where to send user after successful login, and logic if no other page is provided.
+# Where to send user after successful login, and logout, if no other page is provided.
 LOGIN_REDIRECT_URL = 'my_user_profile'
 LOGOUT_REDIRECT_URL = 'homepage'
