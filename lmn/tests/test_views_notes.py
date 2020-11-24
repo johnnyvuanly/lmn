d7637d9379fc80dc8e70bbea4d26d983bbc66006
Alicia <alicia.nguyen@go.minneapolis.edu>
2020-11-24 09:54:41 -0600

    Add Docstring

--- lmn/tests/test_views_notes.py
+++ lmn/tests/test_views_notes.py
@@ -16,18 +16,23 @@ class TestDeleteNotes(TestCase):
         self.client.force_login(user)
 
     def test_delete_notes(self):
-        """During testing user with primary key #1 can login and delete notes. Notes get count in database on specific users """  
+        """During testing user with primary key ID#1 can login and delete notes. Notes get count in database on specific users """  
 
         response = self.client.post(reverse('delete_notes', args=(1,)), follow=True) # note 1 belongs to user 1 therefore, 
         #user 1 can delete note.
 
-        note_count = Note.objects.count()
-        self.assertEqual(2, note_count)# checking database for 2 notes are remaining
-        note_1 = Note.objects.filter(pk=1).first()
+        note_count = Note.objects.count() # Count notes left in database
+        self.assertEqual(2, note_count)# checking and counting database for remaining 2 notes
+        note_1 = Note.objects.filter(pk=1).first() # Select note 1 with primary key #1 first
         self.assertIsNone(note_1)   # note is deleted
 
     def test_delete_someone_else_notes_not_auth(self):
+
+        """"Testing database to check unauthorized user with primary key #5 and status code. If they matched,
+         database prevents the user from cancelling notes. Otherwise, display message. """
+
         response = self.client.post(reverse('delete_notes',  args=(5,)), follow=True)
-        self.assertEqual(403, response.status_code)# Display status code 403 if primary key is not 5
-        note_5 = Note.objects.get(pk=5)
-        self.assertIsNotNone(note_5)    # note still in database
+        self.assertEqual(403, response.status_code)# Compare status code 403 with primary key #5
+        note_5 = Note.objects.get(pk=5) #fetch for ID# 5
+        message = "Test value is none." # error message in case if test case got failed
+        self.assertIsNotNone(note_5, message) # assertIsNotNone() to check that if input value is not none
