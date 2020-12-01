8e067c0de9b6ab036fea57c60327e5d9a1874371
“jy8474np” <jy8474np@go.minnstate.edu>
2020-12-01 12:21:16 -0600

    views_notes.py missing user validations updated.

--- lmn/views/views_notes.py
+++ lmn/views/views_notes.py
@@ -31,7 +31,10 @@ def new_note(request, show_pk):
 @login_required
 def delete_note(request, show_pk):
 
-    show = get_object_or_404(Show, pk=show_pk)
+    note = get_object_or_404(Note, pk=show_pk)
+
+    if note.user !=request.user:
+        return HttpResponseForbidden
 
     if request.method == 'POST' :
         form = NewNoteForm(request.POST, request.FILES, instance=show_pk)
@@ -50,19 +53,28 @@ def delete_note(request, show_pk):
 
     return render(request, 'lmn/notes/note_detail.html' , { 'form': form , 'show': show })
 
+@login_required
 def latest_notes(request):
+
     notes = Note.objects.all().order_by('-posted_date')
     return render(request, 'lmn/notes/note_list.html', { 'notes': notes })
 
+@login_required
+def notes_for_show(request, show_pk):
 
-def notes_for_show(request, show_pk): 
+    note = get_object_or_404(Note, pk=show_pk)
+
+    if note.user !=request.user:
+        return HttpResponseForbidden
+
     # Notes for show, most recent first
     notes = Note.objects.filter(show=show_pk).order_by('-posted_date')
     show = Show.objects.get(pk=show_pk)  
     return render(request, 'lmn/notes/note_list.html', { 'show': show, 'notes': notes })
 
-
+@login_required
 def note_detail(request, note_pk):
+
     note = get_object_or_404(Note, pk=note_pk)
 
     if note.user !=request.user:
