b1f7dd94f6315780f3121f2ad00b71877dd6663e
quinz <quinzel0325@gmail.com>
2020-12-02 13:19:00 -0600

    working tests

--- lmn/admin_views.py
+++ lmn/admin_views.py
@@ -11,8 +11,7 @@ def get_data(request):
     url ='https://app.ticketmaster.com/discovery/v2/events'
     query = {'classificationName': 'music','stateCode' : 'MN' , 'page' : 4 , 'apikey' : key}
     data = requests.get(url, params=query).json()
-    
-    
+    # finds how many results so it can iterate the correct amount of times
     events = data['_embedded']['events']
     event_count = len(events)
 
@@ -23,36 +22,29 @@ def get_data(request):
         venue_city = data['_embedded']['events'][x]['_embedded']['venues'][0]['city']['name']
         # ARTIST DATA
         artist = data['_embedded']['events'][x]['_embedded']['attractions'][0]['name']
-        # SHOW DATA
-        #
-        # show_dateTime = data['_embedded']['events'][x]['dates']['start']['dateTime']
-           
-        ### date and time separated for new model
-       
+        # SHOW DATA 
+        ## date and time separated for new model
         show_date = data['_embedded']['events'][x]['dates']['start']['localDate']
         show_time = data['_embedded']['events'][x]['dates']['start']['localTime']
 
         try:
             # Checking to see if venue exists
             if Venue.objects.filter(name=venue_name).exists():
-                # print(f'{venue_name} already exists')
                 # grabs venue object if exists for show info
-                venue_object = Venue.objects.filter(name=venue_name).get()
-                
+                venue_object = Venue.objects.filter(name=venue_name).get()  
             else:
                 venue_object = Venue(name=venue_name, city=venue_city, state=venue_state)
                 venue_object.save()
             # If artist exists it won't try to add a duplicate
             if Artist.objects.filter(name=artist).exists():
                 artist_object = Artist.objects.filter(name=artist).get()
-                # print(f'{artist} already exists')
             else:
                 artist_object = Artist(name=artist)
                 artist_object.save()
             #
             if Show.objects.filter(show_date=show_date, show_time=show_time, artist=artist_object,venue=venue_object).exists():
+                #ok
                 pass
-                #ok 
             else:
                 show_object = Show(show_date=show_date, show_time=show_time, artist=artist_object,venue=venue_object)
                 show_object.save()
--- lmn/tests/test_API.py
+++ lmn/tests/test_API.py
@@ -5,15 +5,10 @@ import unittest
 from django.test import TestCase
 from django.urls import reverse
 from django.test import override_settings
-
 from django.db.transaction import atomic
-
 from unittest.mock import patch 
-
 from django.contrib.auth.models import User
-
 from ..models import Venue, Artist, Show
-
 from PIL import Image 
 
 
@@ -26,6 +21,13 @@ class TestTicketMasterAPI(TestCase):
         response = self.client.get(reverse('admin_get_data'))
 
         self.assertEqual(200, response.status_code)
+    
+    @patch('requests.Response.json')
+    def test_add_two_data_sets(self, mock_api_json_response):
+
+        mock_api_json_response.side_effect = [{"_embedded":{"events":[{"_embedded":{"venues":[{"name":"First Ave","state":{"stateCode":"MN"},"city":{"name":"Minneapolis"}}],"attractions":[{"name":"Killed BY Kiwis"}]},"dates":{"start":{"localDate":"2020-10-25","localTime":"20:00:00"}}},{"_embedded":{"venues":[{"name":"Babbettes","state":{"stateCode":"MN"},"city":{"name":"Minneapolis"}}],"attractions":[{"name":"!Okay"}]},"dates":{"start":{"localDate":"2020-12-24","localTime":"16:00:00"}}}]}}]
+        response = self.client.get(reverse('admin_get_data'))
+        self.assertEqual(2, Venue.objects.count())
 
     @patch('requests.Response.json')
     def test_no_add_duplicates(self, mock_api_json_response):
@@ -35,8 +37,6 @@ class TestTicketMasterAPI(TestCase):
         response = self.client.get(reverse('admin_get_data'))
         #should only have added 1 venue
         self.assertEqual(1, Venue.objects.count())
-
-
 
 
 
