fecd1ecfb3ddc348c771747f8d83a835d61e4cab
“jy8474np” <jy8474np@go.minnstate.edu>
2020-12-07 21:56:41 -0600

    Corrected show 404 error. Still shows no photo when photo not added in admin

--- lmn/views/views_notes.py
+++ lmn/views/views_notes.py
@@ -38,10 +38,11 @@ def latest_notes(request):
 @login_required
 def notes_for_show(request, show_pk):
 
-    show = get_object_or_404(Note, pk=show_pk)
+    # show = get_object_or_404(Note, pk=show_pk)
 
     # Notes for show, most recent first
-    notes = Note.objects.filter(show=show_pk).order_by('-posted_date')
+    show = Show.objects.get(pk=show_pk)  
+    notes = Note.objects.filter(pk=show_pk).order_by('-posted_date')  
     show = Show.objects.get(pk=show_pk)  
     return render(request, 'lmn/notes/note_list.html', { 'show': show, 'notes': notes })
 
