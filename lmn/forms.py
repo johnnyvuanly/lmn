539adeb329d671aa6a3a0774065f22385e087502
Omar <Omar>
2021-04-25 15:08:16 -0500

    user profile

--- lmn/forms.py
+++ lmn/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Note
+from .models import Note, Profile
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -19,6 +19,10 @@ class NewNoteForm(forms.ModelForm):
         model = Note
         fields = ('title', 'text')
 
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('bio',)
 
 class UserRegistrationForm(UserCreationForm):
 
