fc22221633d35e804fe88baa7300931bdf59cf41
Orion <gn9081ca@go.minneapolis.edu>
2021-05-10 02:38:58 -0500

    commented api_keys

--- lmn/models.py
+++ lmn/models.py
@@ -21,7 +21,7 @@ User._meta.get_field('first_name')._blank = False
 """ A music artist """
 class Artist(models.Model):
     name = models.CharField(max_length=200, blank=False)
-    api_id = models.CharField(max_length=20, blank=False, unique=True)
+    # api_id = models.CharField(max_length=20, blank=False, unique=True, default="default_id")
 
     def __str__(self):
         return f'Name: {self.name}'
@@ -32,7 +32,7 @@ class Venue(models.Model):
     name = models.CharField(max_length=200, blank=False, unique=True)
     city = models.CharField(max_length=200, blank=False)
     state = models.CharField(max_length=2, blank=False)
-    api_id = models.CharField(max_length=20, blank=False, unique=True)
+    # api_id = models.CharField(max_length=20, blank=False, unique=True, default="default_id")
 
     def __str__(self):
         return f'Name: {self.name} Location: {self.city}, {self.state}'
@@ -43,7 +43,7 @@ class Show(models.Model):
     show_date = models.DateTimeField(blank=False)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
-    api_id = models.CharField(max_length=20, blank=False, unique=True)
+    # api_id = models.CharField(max_length=20, blank=False, unique=True, default="default_id")
 
     def __str__(self):
         return f'Artist: {self.artist} At: {self.venue} On: {self.show_date}'
