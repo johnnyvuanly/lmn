eeeb07d90b3047a62aab6185adc76fd9be05f049
Orion <gn9081ca@go.minneapolis.edu>
2021-05-11 13:07:56 -0500

    Beginning work on tests

--- lmn/tests/test_admin_views.py
+++ lmn/tests/test_admin_views.py
@@ -23,8 +23,51 @@ def mock_tickmaster_json():
         mock_data = json.load(test_json)
     return mock_data
 
+# This method will be used by the mock to replace requests.get
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    if args[0] == 'http://someurl.com/test.json':
+        return MockResponse({"key1": "value1"}, 200)
+    elif args[0] == 'http://someotherurl.com/anothertest.json':
+        return MockResponse({"key2": "value2"}, 200)
+
+    return MockResponse(None, 404)
+
+# Our test case class
+class MyGreatClassTestCase(TestCase):
+
+    # We patch 'requests.get' with our own method. The mock object is passed in to our test case method.
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_fetch(self, mock_get):
+
+        file_path = os.path.join(module_dir, 'test.json')
+        with open(file_path) as test_json:
+            test_data = json.load(test_json)
+
+        # Assert requests.get calls
+        fshows = find_shows()
+        json_data = fshows.fetch_json('test.json')
+        self.assertEqual(json_data, {"key1": "value1"})
+        json_data = fshows.fetch_json('http://someotherurl.com/anothertest.json')
+        self.assertEqual(json_data, {"key2": "value2"})
+        json_data = fshows.fetch_json('http://nonexistenturl.com/cantfindme.json')
+        self.assertIsNone(json_data)
+
+        # We can even assert that our mocked method was called with the right parameters
+        self.assertIn(mock.call('http://someurl.com/test.json'), mock_get.call_args_list)
+        self.assertIn(mock.call('http://someotherurl.com/anothertest.json'), mock_get.call_args_list)
+
+        self.assertEqual(len(mock_get.call_args_list), 3)
 class FindShowsTest(TestCase):
 
+    
     def test_find_shows(self):
 
         #self.assertRaises
@@ -37,6 +80,7 @@ class FindShowsTest(TestCase):
         pass
 
     def test_add_venue(self):
+        test_venue = ['U.S. Bank Stadium', 'Minneapolis', 'MN', 'KovZpZAF6ttA']
         pass
 
 
