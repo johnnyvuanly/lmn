7ca1edc34c1e384300b5069ddc1a0c66d2a07914
Orion <gn9081ca@go.minneapolis.edu>
2021-05-12 01:27:24 -0500

    Solved looping errors, commented and cleand code

--- lmn/admin_views.py
+++ lmn/admin_views.py
@@ -19,74 +19,46 @@ def find_shows(request):
     country = 'US'
     search_radius = '50'
     segment_name = 'music'
-
-    # Controls looping over json pagination 
-    page_size = '20'
-    page_count = 3
     locale = '*'
-    iterative_range = range(1,(page_count),1)
 
+    # Controls looping over json pagination
+    page = 1
+    page_size = '20'
 
-    for page in iterative_range:
-        try:
+    # Initial Query
+    query = {'apikey' : ticketmaster_key , 'locale' : locale ,'city': city, 'state': state, 'countryCode': country, 'radius': search_radius, 'segmentName': segment_name, 'size': page_size, 'page' : str(page)}
+
+    try:
+        data = requests.get(root_url, params=query).json()            
+        # Sets page_count to number of pages listed in api response
+        page_count = (data['page']['totalPages'])
+
+        # Loops through each page of the api response
+        for current_page in range(2, page_count):
+            page = current_page
             query = {'apikey' : ticketmaster_key , 'locale' : locale ,'city': city, 'state': state, 'countryCode': country, 'radius': search_radius, 'segmentName': segment_name, 'size': page_size, 'page' : str(page)}
-            data = requests.get(root_url, params=query).json()
-            # module_dir = os.path.dirname(__file__)  # get current directory
-            # file_path = os.path.join(module_dir, './tests/test.json')
-            # with open(file_path) as test_json:
-            #     data = json.load(test_json)
-            
-            # Sets page_count to number of pages listed in api response
-            page_count = (data['page']['totalPages'])
-            logger.debug('Query page: '+ str(page) + ' of '+ str(page_count))
+            data = requests.get(root_url, params=query).json() 
             propogate_db(data)
-        except: 
-            return HttpResponse(f'Error. Response recieved:\n{data}')
+
+    except Exception as e: 
+        return HttpResponse(f'Error: {e}\nResponse recieved:\n{data}')
     return HttpResponse(f'Data recieved. Total pages {str(page_count)}')
 
-
-# get details on each show
-def find_show_details(show_id):
-    # TODO Refresh specific show details using show id
-    pass
-
 def propogate_db(data):
-    # TODO Check if unique id exists for all of these
-
     for show in data['_embedded']['events']:
-        # TODO Check Event ID ['id'] doesn't exist in db
         artist = add_artist( show['id'], show['name'] )
         venue = add_venue( show['_embedded']['venues'][0]['id'] , show['_embedded']['venues'][0]['name'] , show['_embedded']['venues'][0]['city']['name'] , show['_embedded']['venues'][0]['state']['stateCode'])
         add_show( show['id'] , show['dates']['start']['dateTime'] , artist , venue )
 
 def add_venue(venue_id, venue_name, venue_city, venue_state):
-    # TODO Check venue doesn't exist in db using all three attributes
     created_venue, created = Venue.objects.get_or_create(name = venue_name,  city = venue_city, state = venue_state, api_id=venue_id)
-
-    return created_venue
-    
+    return created_venue  
 
 def add_artist(artist_id, artist_name):
-    # TODO Check artist doesn't exist in db using name
     created_artist, created = Artist.objects.get_or_create(api_id = artist_id, name = artist_name)
     return created_artist
     
 
 def add_show(show_id, show_date, show_artist, show_venue):
-    # TODO get venue & artist pk
     created_show, created = Show.objects.get_or_create(api_id = show_id, show_date = show_date, artist = show_artist, venue = show_venue)
     return created_show
-
-
-def is_valid_json(jsonData): 
-#Returns a value error if the json is invalid and returns false, otherwise returns true.
-    try:
-        if (type(jsonData) == dict):
-            return True
-        elif (type(jsonData) == str):
-            json.loads(jsonData)
-            return True
-    except ValueError as err:
-        return False
-
-
